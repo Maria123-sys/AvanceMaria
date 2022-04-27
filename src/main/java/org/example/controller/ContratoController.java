be2332e564e8b1264461833d856822309b525e57
Maria123-sys <mm9744280@gmail.com>
2022-04-27 17:49:53 -0500

    [Frontend] Implementacion de capa ContratoController

---
+++ src/main/java/org/example/controller/ContratoController.java
@@ -0,0 +1,178 @@
+package org.example.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.example.business.ContratoBusiness;
+import org.example.business.ArrendadorBusiness;
+import org.example.business.ArrendatarioBusiness;
+import org.example.business.InmuebleBusiness;
+
+import org.example.entities.Contrato;
+import org.example.util.Message;
+import org.primefaces.event.SelectEvent;
+
+@Named
+@SessionScoped
+public class ContratoController implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private ContratoBusiness contratoBusiness;
+
+	@Inject
+	private ArrendadorBusiness arrendadorBusiness;
+
+	@Inject
+	private ArrendatarioBusiness arrendatioBusiness;
+
+	@Inject
+	private InmuebleBusiness inmuebleBusiness;
+
+	private Contrato contrato;
+	private List<Contrato> contratos;
+	private Contrato contratoSeleccionado;
+	
+	private Long idFiltrar;
+
+	@PostConstruct
+	public void init() {
+		contrato=new Contrato();
+		contratos= new ArrayList<>();
+		contratoSeleccionado= new Contrato();
+		
+		mostrarContratos();
+	}
+	
+	public void mostrarContratos() {
+		try {
+			contratos=contratoBusiness.getAll();
+		}catch (Exception e) {
+			
+		}
+		
+	}
+	
+	public String editarContrato() {
+		String view = "";
+		try {
+			if (this.contratoSeleccionado != null) {
+				this.contrato = contratoSeleccionado;
+
+				view = "/contrato/update";
+			} else {
+				Message.messageInfo("Debe seleccionar un contrato de la lista");
+			}
+		} catch (Exception e) {
+			Message.messageError("Error Editar Contrato :" + e.getMessage());
+		}
+
+		return view;
+	}
+	
+	public String nuevoContrato() {
+		limpiarFormulario();
+		return "insert.xhtml";
+	}
+	
+	public String regresarContrato() {
+		limpiarFormulario();
+		return "list.xhtml";
+	}
+
+	public void limpiarFormulario() {
+		this.contrato = new Contrato();
+	}
+
+	public void consultarContratoPorId() {
+		try {
+
+			contratos = contratoBusiness.getContratoById(idFiltrar);
+			limpiarFormulario();
+			if (contratos.isEmpty()) {
+				Message.messageInfo("No se encontraron contratos con el id ingresado");
+				mostrarContratos();
+			}
+
+		} catch (Exception e) {
+			Message.messageError("Error Consultar :" + e.getMessage());
+		}
+	}
+
+	
+	public void seleccionarContrato(SelectEvent e) {
+		this.contratoSeleccionado = (Contrato) e.getObject();
+	}
+	
+	public ContratoBusiness getContratoBusiness() {
+		return contratoBusiness;
+	}
+
+	public void setContratoBusiness(ContratoBusiness contratoBusiness) {
+		this.contratoBusiness = contratoBusiness;
+	}
+
+	public ArrendadorBusiness getArrendadorBusiness() {
+		return arrendadorBusiness;
+	}
+
+	public void setArrendadorBusiness(ArrendadorBusiness arrendadorBusiness) {
+		this.arrendadorBusiness = arrendadorBusiness;
+	}
+
+	public ArrendatarioBusiness getArrendatioBusiness() {
+		return arrendatioBusiness;
+	}
+
+	public void setArrendatioBusiness(ArrendatarioBusiness arrendatioBusiness) {
+		this.arrendatioBusiness = arrendatioBusiness;
+	}
+
+	public InmuebleBusiness getInmuebleBusiness() {
+		return inmuebleBusiness;
+	}
+
+	public void setInmuebleBusiness(InmuebleBusiness inmuebleBusiness) {
+		this.inmuebleBusiness = inmuebleBusiness;
+	}
+
+	public Contrato getContrato() {
+		return contrato;
+	}
+
+	public void setContrato(Contrato contrato) {
+		this.contrato = contrato;
+	}
+
+	public List<Contrato> getContratos() {
+		return contratos;
+	}
+
+	public void setContratos(List<Contrato> contratos) {
+		this.contratos = contratos;
+	}
+
+	public Contrato getContratoSeleccionado() {
+		return contratoSeleccionado;
+	}
+
+	public void setContratoSeleccionado(Contrato contratoSeleccionado) {
+		this.contratoSeleccionado = contratoSeleccionado;
+	}
+
+	public Long getIdFiltrar() {
+		return idFiltrar;
+	}
+
+	public void setIdFiltrar(Long idFiltrar) {
+		this.idFiltrar = idFiltrar;
+	}
+
+}
